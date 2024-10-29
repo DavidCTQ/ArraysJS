578fe47c418709324d0393af5e20d5f05f718fcf
DavidCTQ <hidenrd@outlook.es>
2024-10-29 06:34:59 -0500

    Probando extensiones

--- js/Array _ Animales.js
+++ js/Array _ Animales.js
@@ -1,9 +1,15 @@
-let animales = ["Perro", "Gato", "Elefante", "Calamar", "Lobo", "Cocodrilo", "Tigre"];
+let animales = [
+  "Perro",
+  "Gato",
+  "Elefante",
+  "Calamar",
+  "Lobo",
+  "Cocodrilo",
+  "Tigre",
+];
 
-for (let i = 0; i < animales.length; i++) 
-    {
-      if (animales[i].length > 5) 
-      {
-        console.log(animales[i]);
-      }
-    }
+for (let i = 0; i < animales.length; i++) {
+  if (animales[i].length > 5) {
+    console.log(animales[i]);
+  }
+}
